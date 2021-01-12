0a862ec5be0c51b22db33ffdd33801bb3efc3823
Ryan Conran <ryan.conran2@gmail.com>
2021-01-11 21:51:14 -0600

    Initial commit, server connection established and base handlebar created.

---
+++ controllers/burgerController.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/add", function(req, res) {
+  console.log(req.body)
+  res.send("ok")
+});
+
+module.exports = router
