522122503d0191abb5395d28e4ec135992779dcd
Ryan Conran <ryan.conran2@gmail.com>
2021-01-12 15:36:45 -0600

    added update and create function on the burger models file

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,23 @@
-
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
 
-}
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
 
+  update: function (cols, condition, cb) {
+    orm.update("burgers", cols, condition, function (res) {
+      cb(res);
+    });
+  },
+};
 
 module.exports = burger;
